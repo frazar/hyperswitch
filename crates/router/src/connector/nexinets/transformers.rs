f07b36f6ab2ac022c886ab1dfbd52983917ec9d6
Francesco Zardi <frazar0@hotmail.it>
2023-10-09 22:30:51 +0200

    Use connector_request_reference_id as reference to the connector
    The reference is passed as the "merchantOrderId" attribute, as described in NexiNets documentation:
    https://developer.nexigroup.com/payengine/en-EU/api/payengine-api-v1/#orders-debit-post

--- crates/router/src/connector/nexinets/transformers.rs
+++ crates/router/src/connector/nexinets/transformers.rs
@@ -27,6 +27,7 @@ pub struct NexinetsPaymentsRequest {
     payment: Option<NexinetsPaymentDetails>,
     #[serde(rename = "async")]
     nexinets_async: NexinetsAsyncDetails,
+    merchant_order_id: Option<String>,
 }
 
 #[derive(Debug, Serialize, Default)]
@@ -179,6 +180,7 @@ impl TryFrom<&types::PaymentsAuthorizeRouterData> for NexinetsPaymentsRequest {
             product,
             payment,
             nexinets_async,
+            merchant_order_id: Some(item.connector_request_reference_id.clone())
         })
     }
 }
